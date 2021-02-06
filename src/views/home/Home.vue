54c29d68705eebc66222e0cb777c4e6a4ac8a284
艾薇儿 <avrillavigne@luxiaoyudegangqin.local>
2021-02-07 00:02:14 +0800

    new

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,21 +1,39 @@
 <template>
     <div id="home">
         <NavBar class="home-div">
-            <div slot="NavBar-left"></div>
-            <div slot="NavBar-center">购物车</div>
-            <div slot="NavBar-right"></div>
-        </NavBar>          
+            <div slot="NavBar-center">购物街</div>
+        </NavBar>         
+        <Swiper>
+            <SwiperItem v-for="(item, index) in banners" :key="index">
+                <a :href="item.link">
+                    <img :src="item.image" alt="">
+                </a>
+            </SwiperItem>
+        </Swiper>
     </div>
 </template>
 
 <script>
 import NavBar from '../../components/common/navbar/NavBar'
+import { getHomemultidata } from '../../network/home' 
+import {Swiper, SwiperItem} from '../../components/common/swiper/index'
 export default {
     data(){
-        return {}
+        return {
+            banners : [],
+            recommends: []
+        }
     },
     components: {
-        NavBar
+        NavBar,
+        Swiper,
+        SwiperItem
+    },
+    created(){
+        getHomemultidata().then(res => {
+            this.banners = res.data.data.banner.list
+            this.recommends = res.data.data.recommend.list
+        })
     }
 }
 </script>
